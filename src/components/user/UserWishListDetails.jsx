bc71cc879ec885fd1389485a0ec4268bf57f33c9
navin1812ku <navin1812ku@gmail.com>
2024-06-24 21:43:59 +0530

    updated wish list component

--- src/components/user/UserWishListDetails.jsx
+++ src/components/user/UserWishListDetails.jsx
@@ -1,25 +1,30 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import productImage from '../../../public/product.jpg';
 
 const UserWishListDetails = () => {
-    const { id } = useParams();    
+    const { id } = useParams();
     const [wishList, setWishList] = useState([]);
     const [successMessage, setSuccessMessage] = useState('');
+    const [wishListName, setWishListName] = useState('');
     const [isHavingProduct, setIsHavingProduct] = useState(false);
 
     useEffect(() => {
         const fetchList = async (id) => {
             try {
                 const token = localStorage.getItem(`token`);
-                const response = await axios.get(`http://localhost:8081/user/wishListDetsils/${id}`,
+                const response = await axios.get(`http://localhost:8081/user/wishListById/${id}`,
                     {
                         headers: {
                             Authorization: `Bearer ${token}`
                         }
                     }
                 );
+                console.log(response.data);
                 if (response.data.success) {
-                    setWishList(response.data.wishList);
+                    setWishListName(response.data.wishList.name);
+                    setWishList(response.data.wishList.products);
                     setIsHavingProduct(false);
                 } else {
                     setIsHavingProduct(true);
@@ -27,16 +32,41 @@ const UserWishListDetails = () => {
                 }
             }
             catch (error) {
-                console.log(`Failed to remove list`);
+                console.log(`Failed to fetch wish list details`, error);
             }
         };
 
         fetchList(id);
     }, [id]);
 
-    return(
+    return (
         <div>
-            <h1>Products</h1>
+            <div className=" font-bold mt-5 text-5xl flex relative">
+                <h1 className=" text-left">Wish List</h1>
+                <h1 className=" absolute ml-72 text-red-800">{wishListName}</h1>
+            </div>
+            <div className="w-full max-w-4xl mt-4 ml-72">
+                {wishList && wishList.length > 0 ? (
+                    wishList.map((product, index) => (
+                        <div key={index} className=" bg-slate-100 shadow-md rounded-2xl p-4 mb-4 flex items-center">
+                            <img src={productImage} alt={product.title} className=" rounded-3xl w-72 p-3" />
+                            <div className="mt-10 space-y-1.5 ml-12">
+                                <h2>{product.title}</h2>
+                                <p>{product.description}</p>
+                                <p><span className="font-bold">Price</span>: {product.price}</p>
+                                <p><span className="font-bold">Category</span>: {product.category}</p>
+                                <p><span className="font-bold">Brand</span>: {product.brand}</p>
+                                <div className="">
+                                    <button className="mr-8 mt-5 hover:text-orange-600 font-bold text-lg">Remove</button>
+                                    <button className="mr-8 mt-5 hover:text-orange-600 font-bold text-lg">Buy</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>No products in this wishlist.</p>
+                )}
+            </div>
         </div>
     )
 };
